1ee0ae21749d164d9689a53d3d6d2b923790ec5d
ALEVICENZO <alevicenzo@gmail.com>
2025-06-02 13:51:26 -0300

    Atualiza run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,11 @@
 
 export PYTHONWARNINGS="ignore:numpy"
 
+# --- AJUSTE CRÍTICO: Ativar o ambiente virtual ---
+source /opt/venv/bin/activate
+# -------------------------------------------------
+
+# Agora, 'python3' e 'pytest' usarão as versões do ambiente virtual
 command="python3 -m pytest --cov=cd4ml --cov-report html:cov_html test"
 
 echo "$command"
@@ -12,4 +17,5 @@ eval "$command"
 echo
 echo Flake8 comments:
 # extend-ignore T0001 ignores print() statements in project
+# 'flake8' também usará a versão do ambiente virtual, se instalada lá.
 flake8 --extend-ignore T001 --max-line-length=120 cd4ml
